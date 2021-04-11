bca2e20736e87c055632fea3e739f61e68e337f7
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-11 14:09:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-function App() {
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
