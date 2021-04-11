e4ef54c2bcf017dada024e393bf3b64aca8d45a7
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-12 00:56:07 +0100

    Refactor InfoBox

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Heading } from "@chakra-ui/react";
 import { ImBriefcase } from "react-icons/all";
 
+import InfoList from "./InfoList";
+import education from "../data/education";
+
+
 const Education = () => {
   return (
     <>
-      <Heading fontWeight="thin" borderBottom="1px" p={4}>
+      <Heading fontWeight="thin" borderBottom="1px" p={4} m={4}>
         Education <ImBriefcase style={{ display: "inline" }} />
       </Heading>
+      <InfoList data={education} />
     </>
   );
 };
