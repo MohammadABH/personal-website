4fb67dc8b44c168485c1c6d79c289bffe73e01f8
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-12 00:55:27 +0100

    Add education data

---
+++ src/data/education.js
@@ -0,0 +1,24 @@
+import kcl from "../images/kcl_logo.svg";
+
+const education = [
+  {
+    title: "King's College London",
+    description: "Studying Computer Science BSc",
+    image: kcl,
+    yearRange: "2019-2022",
+    labels: [
+      "Java",
+      "C++",
+      "Scala",
+      "Ruby on Rails",
+      "Data Structures",
+      "Databases",
+      "Discrete Mathematics",
+      "Programming Language Paradigms",
+      "Concurrency",
+      "Computer Architecture",
+    ],
+  },
+];
+
+export default education;
