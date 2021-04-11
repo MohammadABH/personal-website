2e72e9df17ada4b6b5e54f45f5b1258ce98b6d88
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-11 17:03:01 +0100

    Add ability to switch between themes

---
+++ src/components/ColourModeToggle.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Box, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const ColourModeToggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Box onClick={toggleColorMode}>
+      {colorMode === "light" ? <MoonIcon fontSize="200%" /> : <SunIcon fontSize="200%" />}
+    </Box>
+  );
+};
+
+export default ColourModeToggle;
