2d93e83a0c69e30d5c34726ed92299f60ed1e540
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-11 23:09:07 +0100

    Add education component

---
+++ src/components/Education.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { ImBriefcase } from "react-icons/all";
+
+const Education = () => {
+  return (
+    <>
+      <Heading fontWeight="thin" borderBottom="1px" p={4}>
+			Education <ImBriefcase  style={{display: "inline"}} />
+      </Heading>
+    </>
+  );
+};
+
+export default Education;
