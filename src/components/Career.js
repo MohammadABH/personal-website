8b6168ae7d215bde89e51c74e9face88382ecd2f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-12 00:56:19 +0100

    Refactor Career.js

--- src/components/Career.js
+++ src/components/Career.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Heading } from "@chakra-ui/react";
 import { ImBooks } from "react-icons/all";
 
+import InfoList from "./InfoList";
+import career from "../data/career";
+
+
 const Career = () => {
   return (
     <>
-      <Heading fontWeight="thin" borderBottom="1px" p={4}>
+      <Heading fontWeight="thin" borderBottom="1px" p={4} m={4}>
         Career <ImBooks style={{ display: "inline" }} />
       </Heading>
+      <InfoList data={career} />
     </>
   );
 };
