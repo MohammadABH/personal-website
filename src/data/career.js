33a62748ecf9b65c098c563d71b3b4d0ac50eff2
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-12 00:55:36 +0100

    Add career data

---
+++ src/data/career.js
@@ -0,0 +1,13 @@
+import intuit from "../images/intuit_logo.jpg";
+
+const career = [
+  {
+    title: "Intuit",
+    description: "Software Engineer Intern",
+    image: intuit,
+    yearRange: "June 2021 - September 2021",
+    labels: [],
+  },
+];
+
+export default career;
