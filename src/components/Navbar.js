e289213bafd5b3304a5168d5e3b6e265cdd2a284
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-11 17:03:54 +0100

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Logo from "./Logo";
+import NavbarContainer from "./NavbarContainer";
+import NavbarLinks from "./NavbarLinks";
+
+import NavbarToggle from "./NavbarToggle";
+
+const Navbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <NavbarContainer {...props}>
+      <Logo w="100px" />
+      <NavbarToggle toggle={toggle} isOpen={isOpen} />
+      <NavbarLinks isOpen={isOpen} />
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
