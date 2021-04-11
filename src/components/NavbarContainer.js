543ff1b0826b23ae96fe859bf81c75200978349f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-11 17:03:38 +0100

    Add navbar container component

---
+++ src/components/NavbarContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+
+const NavbarContainer = ({ children, ...props }) => {
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      mb={8}
+      p={8}
+      boxShadow="lg"
+      bg={["primary.500", "primary.500", "transparent", "transparent"]}
+			padding="1.5%"
+      {...props}
+    >
+      {children}
+    </Flex>
+  );
+};
+
+export default NavbarContainer;
