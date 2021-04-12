24a28ca9e462b7f6bdfcc0839a093546ae757179
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-12 09:48:42 +0100

    Add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+import kcl from "../images/kcl_logo.svg";
+
+const projects = [
+  {
+    title: "Airbnb Desktop Application",
+    description:
+      "Designed and implemented an Airbnb desktop application in Java using the JavaFX framework along with 3 other team members in a university project.",
+    image: kcl,
+    yearRange: "2019",
+    labels: ["Java", "JavaFX", "CSS", "Intellij"],
+  },
+  {
+    title: "Manage your Expenses",
+    description:
+      "Created a fully functioning budgeting/expenses web application using React JS, Redux, Firebase and deployed using Heroku.",
+    image: kcl,
+    yearRange: "2019",
+    labels: ["JavaScript", "React", "Redux", "Firebase", "Heroku"],
+  },
+];
+
+export default projects;
