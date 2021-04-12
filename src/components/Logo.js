b872c3cbb163606437df9a1ca05543aac969f16f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 00:14:32 +0100

    Update logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+import { Link, Image } from "@chakra-ui/react";
 
-import avatar from "../images/avatar.jpg"
+import avatar from "../images/avatar.jpg";
 
 const Logo = () => {
-	return (
-		<Box>
-			<Image src={avatar} borderRadius="50%" inlineSize="20"/> 
-		</Box>
-	);
+  return (
+    <Link href="https://www.linkedin.com/in/mohammad-albinhassan/" isExternal>
+      <Image src={avatar} borderRadius="50%" inlineSize="20" />
+    </Link>
+  );
 };
 
 export default Logo;
