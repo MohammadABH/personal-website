515025e7c16ec123e7fff8d80bde95a94524e78f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 14:52:38 +0100

    Add tooltips to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, Center, HStack } from "@chakra-ui/react";
+import { Box, Center, HStack, Tooltip } from "@chakra-ui/react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/all";
 
 const Footer = () => {
   return (
-    <Center >
+    <Center>
       <HStack
         as="footer"
         isInline
         p={4}
         maxW={800}
         mx="auto"
-				mt={10}
+        mt={10}
         alignContent="center"
         position="absolute"
         textAlign="center"
       >
+				<Tooltip label="Thats me" hasArrow arrowSize={15}>
         <Box>©️ {new Date().getFullYear()} Mohammad ABH</Box>
+				</Tooltip>
 
         <HStack>
-          <Link
-            to={{ pathname: "https://github.com/MohammadABH/" }}
-            target="_blank"
-          >
-            <AiFillGithub fontSize="150%" />
-          </Link>
+          <Tooltip label="GitHub" hasArrow arrowSize={15}>
+            <Link
+              to={{ pathname: "https://github.com/MohammadABH/" }}
+              target="_blank"
+            >
+              <AiFillGithub fontSize="150%" />
+            </Link>
+          </Tooltip>
 
-          <Link
-            to={{
-              pathname: "https://www.linkedin.com/in/mohammad-albinhassan/",
-            }}
-            target="_blank"
-          >
-            <AiFillLinkedin fontSize="150%" />
-          </Link>
+          <Tooltip label="LinkedIn" hasArrow arrowSize={15}>
+            <Link
+              to={{
+                pathname: "https://www.linkedin.com/in/mohammad-albinhassan/",
+              }}
+              target="_blank"
+            >
+              <AiFillLinkedin fontSize="150%" />
+            </Link>
+          </Tooltip>
         </HStack>
       </HStack>
     </Center>
