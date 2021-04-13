f6334b1f28c7a7665907f406a7c3138dd26b9c39
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 01:54:11 +0100

    Update logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,10 +3,10 @@ import { Link, Image } from "@chakra-ui/react";
 
 import avatar from "../images/avatar.jpg";
 
-const Logo = () => {
+const Logo = ({ size="20%" }) => {
   return (
     <Link href="https://www.linkedin.com/in/mohammad-albinhassan/" isExternal>
-      <Image src={avatar} borderRadius="50%" inlineSize="20" />
+      <Image src={avatar} borderRadius="50%" inlineSize={size} />
     </Link>
   );
 };
