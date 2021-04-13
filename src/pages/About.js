215078f88fa997964d7576ecb7b4d437bfb8277c
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 16:20:44 +0100

    Add UserCard to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Container } from "@chakra-ui/react";
 
+import avatar from "../images/avatar.jpg";
 import Career from "../components/Career";
 import Certificates from "../components/Certificates";
 import CustomHeading from "../components/CustomHeading";
 import Education from "../components/Education";
+import UserCard from "../components/UserCard";
 
 const About = () => {
+  const text = [
+    "Computer Science student at King's College London",
+    "Programmer",
+    "Full-stack Engineer",
+    "Backend Engineer",
+  ];
   return (
     <>
       <CustomHeading title={"About Me"} />
       <Container maxW="container.xl" mb={20}>
+        <UserCard
+          name="Mohammad Albinhassan"
+          badge="Owner"
+          text={text}
+          avatar={avatar}
+        />
         <Education />
         <Career />
-				<Certificates />
+        <Certificates />
       </Container>
     </>
   );
