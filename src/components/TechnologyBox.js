a1ddc35a55eb827af69e0fcefeb5c46896e2ef0f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 11:40:04 +0100

    Add tolltip

--- src/components/TechnologyBox.js
+++ src/components/TechnologyBox.js
@@ -1,30 +1,38 @@
 import React from "react";
-import { Box, LinkBox, LinkOverlay, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  LinkBox,
+  LinkOverlay,
+  Tooltip,
+  useColorMode,
+} from "@chakra-ui/react";
 
 const TechnologyBox = ({ techItem }) => {
   const { colorMode } = useColorMode();
 
   return (
-    <LinkBox
-      _hover={{ transform: "translateY(-4px)", shadow: "sm" }}
-      transition="all 0.25s"
-      transition-timing-function="spring(1 100 10 10)"
-    >
-      <LinkOverlay href={techItem.url} textDecoration="none" isExternal>
-        <Box
-          backgroundColor={colorMode === "light" ? "white" : "gray.900"}
-          maxW="sm"
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          width="110%"
-					p={4}
-        >
-          {techItem.icon()}
-          {techItem.name}
-        </Box>
-      </LinkOverlay>
-    </LinkBox>
+    <Tooltip label={techItem.hover} hasArrow arrowSize={15}>
+      <LinkBox
+        _hover={{ transform: "translateY(-4px)", shadow: "sm" }}
+        transition="all 0.25s"
+        transition-timing-function="spring(1 100 10 10)"
+      >
+        <LinkOverlay href={techItem.url} textDecoration="none" isExternal>
+          <Box
+            backgroundColor={colorMode === "light" ? "white" : "gray.900"}
+            maxW="sm"
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            width="110%"
+            p={4}
+          >
+            {techItem.icon()}
+            {techItem.name}
+          </Box>
+        </LinkOverlay>
+      </LinkBox>
+    </Tooltip>
   );
 };
 
