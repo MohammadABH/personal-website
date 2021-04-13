28481ee75d6cfa709fbc71f842515a275c72e76e
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 16:23:25 +0100

    Add Fade animation

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Avatar, Link, Tooltip } from "@chakra-ui/react";
+import { Avatar, Fade, Link, Tooltip } from "@chakra-ui/react";
 
 import avatar from "../images/avatar.jpg";
 
 const Logo = ({ size = "xl" }) => {
   return (
-    <Tooltip label="LinkedIn" hasArrow arrowSize={15}>
-      <Link href="https://www.linkedin.com/in/mohammad-albinhassan/" isExternal>
-        <Avatar src={avatar} size={size} />
-      </Link>
-    </Tooltip>
+    <Fade in={true}>
+      <Tooltip label="LinkedIn" hasArrow arrowSize={15}>
+        <Link
+          href="https://www.linkedin.com/in/mohammad-albinhassan/"
+          isExternal
+        >
+          <Avatar src={avatar} size={size} />
+        </Link>
+      </Tooltip>
+    </Fade>
   );
 };
 
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,27 +1,29 @@
 import React from "react";
 import TextLoop from "react-text-loop";
-import { Avatar, Badge, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Badge, Box, Fade, Flex, Text } from "@chakra-ui/react";
 
 const UserCard = ({ avatar, badge, text, name }) => {
   return (
-    <Flex borderWidth="1px" borderRadius="12px" padding={4} m={4}>
-      <Avatar src={avatar} />
-      <Box ml="3">
-        <Text fontWeight="bold">
-          {name}
-          <Badge ml="1" colorScheme="green">
-            {badge}
-          </Badge>
-        </Text>
-        <Flex width="auto" fontSize="sm" textAlign="center" position="fixed">
-          <TextLoop>
-            {text.map((el, index) => (
-              <span key={index}>{el}</span>
-            ))}
-          </TextLoop>
-        </Flex>
-      </Box>
-    </Flex>
+    <Fade in={true}>
+      <Flex borderWidth="1px" borderRadius="12px" padding={4} m={4}>
+        <Avatar src={avatar} />
+        <Box ml="3">
+          <Text fontWeight="bold">
+            {name}
+            <Badge ml="1" colorScheme="green">
+              {badge}
+            </Badge>
+          </Text>
+          <Flex width="auto" fontSize="sm" textAlign="center">
+            <TextLoop>
+              {text.map((el, index) => (
+                <span key={index}>{el}</span>
+              ))}
+            </TextLoop>
+          </Flex>
+        </Box>
+      </Flex>
+    </Fade>
   );
 };
 
