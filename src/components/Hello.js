66dc77da74e5ac15dcb8207820de361612ca756d
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 01:54:28 +0100

    Add content to Hello.js

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Center, Box, Text } from "@chakra-ui/react";
+import TextLoop from "react-text-loop";
+
+import CustomHeading from "./CustomHeading";
 
 const Hello = () => {
-	return (
-		<div>
-			Hello World
-		</div>
-	);
+  return (
+    <Center>
+      <Box>
+        <CustomHeading title="Welcome!" />
+        <Box width="100%" mt={4}>
+          <Text fontSize="3xl" textAlign="center">
+            My name is Mohammad, I'm{" "}
+            <TextLoop>
+              <span>a Computer Science student at KCL</span>
+              <span>a programmer</span>
+              <span>a genius?</span>
+              <span>a prodigy?</span>
+              <span>ok a meh dude</span>
+            </TextLoop>{" "}
+            based in London, UK
+          </Text>
+        </Box>
+      </Box>
+    </Center>
+  );
 };
 
 export default Hello;
