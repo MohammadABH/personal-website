6778841ad2b49d240d33f20a356a4160b689a9d7
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 15:15:52 +0100

    Add audio click noise

--- src/components/ColourModeToggle.js
+++ src/components/ColourModeToggle.js
@@ -1,13 +1,25 @@
 import React from "react";
+import useSound from "use-sound";
 import { Box, Tooltip, useColorMode, useToast } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+import click from "../sounds/click.mp3";
 
 const ColourModeToggle = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const toast = useToast();
 
+  const [play] = useSound(click, {
+    volume: 0.05,
+    sprite: {
+      on: [0, 300],
+      off: [500, 300],
+    },
+  });
+
   const handleClick = () => {
     toggleColorMode();
+    colorMode === "dark" ? play({ id: "on" }) : play({ id: "off" });
     toast({
       title: `Switched to ${colorMode === "light" ? "Dark" : "Light"} mode`,
       status: "info",
