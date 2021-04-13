6144c83ccb19e33be3c518d627b93e5b10dc58a2
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-13 17:48:39 +0100

    Add terminal page

---
+++ src/pages/TerminalPage.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { Center } from "@chakra-ui/react";
+import Terminal from "terminal-in-react";
+
+import CustomHeading from "../components/CustomHeading";
+
+const TerminalPage = () => {
+  const commands = {
+    name: {
+      method: (args, print, runCommand) => {
+        print(`Mohammad Albinhassan`);
+      },
+      options: [
+        {
+          name: "name",
+          description: "my name",
+          defaultValue: "Mohammad Albinhassan",
+        },
+      ],
+    },
+    age: {
+      method: (args, print, runCommand) => {
+        print(`19`);
+      },
+      options: [
+        {
+          name: "age",
+          description: "my age",
+          defaultValue: "19",
+        },
+      ],
+    },
+    email: {
+      method: (args, print, runCommand) => {
+        print(`mohammad.nb@outlook.com`);
+      },
+      options: [
+        {
+          name: "email",
+          description: "my email",
+          defaultValue: "mohammad.nb@outlook.com",
+        },
+      ],
+    },
+    study: {
+      method: (args, print, runCommand) => {
+        print(`Computer Science at King's College London`);
+      },
+      options: [
+        {
+          name: "study",
+          description: "what i Study",
+          defaultValue: "Computer Science at King's College London",
+        },
+      ],
+    },
+  };
+
+  return (
+    <>
+      <CustomHeading title="Terminal" />
+      <Center mt={10} mb={10}>
+        <Terminal
+          allowTabs={true}
+          color="green"
+          backgroundColor="black"
+          barColor="black"
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={commands}
+          descriptions={{
+            name: "my name",
+            age: "my age",
+            study: "what I study",
+            email: "my email",
+            alert: "alert",
+            popup: "alert",
+          }}
+          msg={
+            'Welcome to the terminal! Type "help" to see a list of all commands'
+          }
+        />
+      </Center>
+    </>
+  );
+};
+
+export default TerminalPage;
