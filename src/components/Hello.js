d737f86857cf6cd1e9aec34677b559cca6544ec4
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-14 16:43:37 +0100

    Add responsive support for animated text

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,40 +1,39 @@
-import React from "react";
-import { Alert, Box, Center, Container, Fade, Tooltip } from "@chakra-ui/react";
-import { GiPartyPopper } from "react-icons/all";
+import React, { useState } from "react";
+import { Box, Center, Fade } from "@chakra-ui/react";
 import TextLoop from "react-text-loop";
+import { useMediaQuery } from "react-responsive";
 
 import CustomHeading from "./CustomHeading";
+import Welcome from "./Welcome";
+import AnimatedText from "./AnimatedText";
 
 const Hello = () => {
+  const isMobile = useMediaQuery({ maxWidth: 750 });
+
+  const bigScreenText = [
+    "a Computer Science student at KCL",
+    "a programmer",
+    "a genius?",
+    "a prodigy?",
+    "ok a meh dude",
+  ];
+  const mobileText = [
+    "a Computer Science student at KCL",
+    "a Full-Stack Engineer",
+    "a Backend Engineer",
+  ];
+
+  const text = isMobile ? mobileText : bigScreenText;
   return (
     <Fade in={true}>
       <Center>
-        <Box width="100%">
+        <Box width="auto">
           <CustomHeading title="Hello World👋🌍" />
-          <Box width="100%" p={4} mt={4} fontSize="3xl" textAlign="center">
-            My name is Mohammad, I'm{" "}
-            <TextLoop>
-              <span>a Computer Science student at KCL</span>
-              <span>a programmer</span>
-              <span>a genius?</span>
-              <span>a prodigy?</span>
-              <span>ok a meh dude</span>
-            </TextLoop>{" "}
-            based in London, UK
+          <Box width="auto" p={4} mt={4} fontSize="3xl" textAlign="center">
+            My name is Mohammad, I'm <AnimatedText text={text} /> based in
+            London, UK
           </Box>
-          <Container>
-            <Center m={4} w="100%" textAlign="center">
-              <Alert m={4} status="success" variant="left-accent">
-                <Tooltip label="Hooray!" hasArrow arrowSize={15}>
-                  <Box mr={4}>
-                    <GiPartyPopper />
-                  </Box>
-                </Tooltip>
-                Welcome to my personal website, have a look around! <br />
-                Email me: mohammad.nb@outlook.com
-              </Alert>
-            </Center>
-          </Container>
+          <Welcome />
         </Box>
       </Center>
     </Fade>
